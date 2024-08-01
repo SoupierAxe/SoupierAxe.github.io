4626ae9ed629342a9d415ac0b8d4675f29c61b4b
SoupierAxe <Biu.bayeux@gmail.com>
2024-08-01 02:02:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("meuBotao").addEventListener("click", function() {
 
     // Cria um novo elemento de imagem
     var img = document.createElement("img");
-    img.src = "https://images.app.goo.gl/F1BJ4ufJNZFHgJYi"; // Substitua pela URL da sua imagem
+    img.src = "https://tse4.mm.bing.net/th?id=OIP.6cBTiqWzzJH-mvcJKVSkZwHaE8&pid=Api&P=0&h=180"; // Substitua pela URL da sua imagem
     img.alt = "Descrição da Imagem";
     img.style.maxWidth = "100%"; // Ajuste para a largura máxima do container
 
